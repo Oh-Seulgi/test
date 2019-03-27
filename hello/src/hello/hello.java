43814e401f40d5497fb3134759166f010c03dd80
tmfrl5584 <tmfrl5584@naver.com>
2019-03-27 12:04:28 +0900

    2

--- hello/src/hello/hello.java
+++ hello/src/hello/hello.java
@@ -4,6 +4,6 @@ public class hello {
 	public static void main(String[] args) {
 		System.out.println("hello world");
 		System.out.print("World's End Dancehall");
-		System.out.print("Earth's End Dancehall");
+		System.out.print("hello End Dancehall");
 	}
 }
